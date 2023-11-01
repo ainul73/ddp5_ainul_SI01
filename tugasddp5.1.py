4626bbdf1b591aeb84d6506109a165b103a1930c
Ainul <149448426+ainul73@users.noreply.github.com>
2023-11-01 12:35:11 +0700

    Add files via upload

---
+++ tugasddp5.1.py
@@ -0,0 +1,8 @@
+#info kendaraan
+kendaraan_info=["motor","sepeda motor","180","hitam","2"]
+print(kendaraan_info)
+
+kendaraan_info.append(20000000)
+kendaraan_info.append("matic")
+kendaraan_info.append("honda beat")
+print(kendaraan_info)
